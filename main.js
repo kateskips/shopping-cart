7c58124f1a70750dfbd9e7f6d16665b6975007a1
Kate Follett <git@klgf.dev>
2022-10-06 00:15:18 -0400

    update functionality

--- main.js
+++ main.js
@@ -51,35 +51,41 @@ let generateShop = () => {
             </div>
         </div>
     `
-    }).join("")); 
+    }).join(""));
 };
 
 generateShop();
 
 let increment = (id) => {
     let selectedItem = id
-    let search = basket.find((x)=> x.id === selectedItem.id)
+    let search = basket.find((x) => x.id === selectedItem.id)
 
-    if(search === undefined){
+    if (search === undefined) {
         basket.push({
             id: selectedItem.id,
             item: 1
         })
-    }  else {
+    } else {
         search.item += 1;
-    } 
-    
-    console.log(basket)
+    }
+
+    //console.log(basket)
+    update(selectedItem.id)
 }
 let decrement = (id) => {
     let selectedItem = id
-    let search = basket.find((x)=> x.id === selectedItem.id)
+    let search = basket.find((x) => x.id === selectedItem.id)
 
-    if(search.item === 0) return
-      else {
+    if (search.item === 0) return
+    else {
         search.item -= 1;
-    } 
-    
-    console.log(basket)
+    }
+
+    //console.log(basket)
+    update(selectedItem.id)
 }
-let update = () => {}
+let update = (id) => {
+    let search = basket.find((x) =>  x.id === id )
+    console.log(search.item)
+    document.getElementById(id).innerHTML = search.item
+}

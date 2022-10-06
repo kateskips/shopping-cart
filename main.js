36ac3cae256797a4a9030c89819d215c74b7522b
Kate Follett <git@klgf.dev>
2022-10-06 02:43:14 -0400

    local storage works

--- main.js
+++ main.js
@@ -31,11 +31,14 @@ let shopItemsData = [
     },
 ];
 
-let basket = [];
+let basket = JSON.parse(localStorage.getItem("data")) ||[];
+//JSON.parse(localStorage.getItem("data")) || []
+
 
 let generateShop = () => {
     return (shop.innerHTML = shopItemsData.map((x) => {
         let { id, name, price, desc, img } = x;
+        let search = basket.find((x)=> x.id === id) || []
         return `<div id=product-id-${id} class="item">
             <img width="219" height="220" src=${img} alt="">
             <div class="details">
@@ -45,7 +48,7 @@ let generateShop = () => {
                     <h2>$ ${price}</h2>
                     <div class="buttons"></div>
                     <i onClick="decrement(${id})" class="bi bi-dash-lg"></i>
-                    <div id=${id} class="quantity">0</div>
+                    <div id=${id} class="quantity">${search.item === undefined? 0: search.item }</div>
                     <i onClick="increment(${id})" class="bi bi-plus-lg"></i>
                 </div>
             </div>
@@ -55,6 +58,7 @@ let generateShop = () => {
 };
 
 generateShop();
+
 
 let increment = (id) => {
     let selectedItem = id
@@ -68,6 +72,7 @@ let increment = (id) => {
     } else {
         search.item += 1;
     }
+    localStorage.setItem("data", JSON.stringify(basket))
 
     //console.log(basket)
     update(selectedItem.id)
@@ -80,7 +85,7 @@ let decrement = (id) => {
     else {
         search.item -= 1;
     }
-
+    localStorage.setItem("data", JSON.stringify(basket))
     //console.log(basket)
     update(selectedItem.id)
 }
@@ -95,3 +100,6 @@ let calculation = () => {
     let cartIcon = document.getElementById("cartAmount")
     cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0)
 }
+
+
+calculation();
